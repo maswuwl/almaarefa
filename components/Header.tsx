c014e740565f3338295bbcf68325dbd8bcead7ab
maswuwl <maswuwl2030@gmail.com>
2025-10-25 00:41:25 +0300

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+// components/Header.tsx
+
+export default function Header() {
+  return (
+    <header style={{ background: '#0070f3', padding: '1rem', color: 'white', textAlign: 'center' }}>
+      <h1>منصة المعرفة</h1>
+    </header>
+  );
+}
