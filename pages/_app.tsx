0eaf4b5159a8f01246f258843783c68c870c2a75
maswuwl <maswuwl2030@gmail.com>
2025-10-25 08:19:21 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,8 @@
 // pages/_app.tsx
 
 import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import '../public/styles/global.css';
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (!token && router.pathname !== '/login') {
-      router.push('/login');
-    }
-  }, []);
-
   return <Component {...pageProps} />;
 }
